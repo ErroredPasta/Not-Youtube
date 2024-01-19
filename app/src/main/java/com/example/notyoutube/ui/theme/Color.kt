dc199e9dc168be2a8627640efe50f818cda8ef29
ErroredPasta <ForeignServices3132@pm.me>
2024-01-19 17:19:11 +0900

    feat: introduce style

--- app/src/main/java/com/example/notyoutube/ui/theme/Color.kt
+++ app/src/main/java/com/example/notyoutube/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.notyoutube.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightPrimary = Color(0xff2e3c60)
+val LightSecondary = Color(0xff8fa2d6)
+val LightTertiary = Color(0xff3457b7)
+val LightBackground = Color(0xffeff1f6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPrimary = Color(0xff9fadd1)
+val DarkSecondary = Color(0xff293c70)
+val DarkTertiary = Color(0xff486bcb)
+val DarkBackground = Color(0xff090b10)
